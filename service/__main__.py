07870058787e817239a9cb193ef48aa0a6572498
Oleg Pronin <o.pro12@mail.ru>
2022-10-24 18:04:09 +0300

    chore:  add app

--- service/__main__.py
+++ service/__main__.py
@@ -1,10 +1,12 @@
 import logging
+from service.countries import app
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 def main():
-    logger.info('Hello world')
+    logger.info('Start')
+    app.run()
 
 if __name__ == '__main__':
     main()
