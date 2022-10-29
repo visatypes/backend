140d267cf52bbeb558bf564a4a58372920bd185c
Oleg Pronin <o.pro12@mail.ru>
2022-10-29 08:45:04 +0300

    add blueprint

--- service/__main__.py
+++ service/__main__.py
@@ -1,9 +1,29 @@
 import logging
 
-from service.countries.views import app
+from flask import Flask
+from pydantic import ValidationError
+
+from service.countries.views import country_view
+from service.errors import AppError
+
+app = Flask(__name__)
+
+app.register_blueprint(country_view, url_prefix='/api/countries')
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+
+def handle_app_error(error: AppError):
+    return {'error': str(error)}, error.code
+
+
+def handle_valid_error(error: ValidationError):
+    return {'error': str(error)}, 400
+
+
+app.register_error_handler(AppError, handle_app_error)
+app.register_error_handler(ValidationError, handle_valid_error)
 
 
 def main():
