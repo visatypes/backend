15402a0a327fddf0b78c3dedf16330f52478203b
Oleg Pronin <o.pro12@mail.ru>
2022-10-31 18:27:16 +0300

    feature: add db, models

---
+++ service/countries/db.py
@@ -0,0 +1,12 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+db_url = os.environ['DB_URL']
+engine = create_engine(db_url)
+db_session = scoped_session(sessionmaker(bind=engine))
+
+Base = declarative_base()
+Base.query = db_session.query_property()
